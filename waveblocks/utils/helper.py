3cdbdd30402f9dd1f69001fd814ce17490680de8
Josue Page <pv.josue@gmail.com>
2022-11-19 00:58:49 +0100

    Fixed find free GPU function

--- waveblocks/utils/helper.py
+++ waveblocks/utils/helper.py
@@ -11,7 +11,7 @@ def get_free_gpu():
     """
     
     _, output = subprocess.getstatusoutput(
-        "nvidia-smi -q -d Memory |grep -A4 GPU|grep Free"
+        "nvidia-smi -q -d Memory |grep -A5 GPU|grep Free"
     )
     memory_available = [int(s) for s in output.split() if s.isdigit()]
     return np.argmax(memory_available)
